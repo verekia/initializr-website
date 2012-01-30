e4b8bbd3ed3f71dbd562c8636fd6b829b7154c1a
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2012-01-30 18:12:17 +0100

    Replaces jquerymin + jquerydev by jquery

--- js/script.js
+++ js/script.js
@@ -1,32 +1,87 @@
 $(function(){
+
+		
+	
+	/************
+	   VARIABLES
+	 ************/
 	
 	var params;
 	var modules = [];
 
+	/**********
+	   EVENTS
+	 **********/	
+
 	$('input').click(function(){
-		updateUrls();
-		updateModules();
+		update();
 	});
 
 	
+	/*********
+	   LOGIC
+	 *********/
+	
+	function update(){
+		updateModules();
+		updateUrls();
+	}
+	
 	function updateModules(){
-		
+		modules = [];
+		$('input').each(function(){
+			if ($(this).is(':checked'))
+				modules.push($(this).val());
+		});
+		replaceSpecialModules();
+	}
+	
+	function replaceSpecialModules(){
+		if (modules.indexOf('jquerymin') != -1 && modules.indexOf('jquerydev') != -1){
+			modules.remove('jquerymin');
+			modules.remove('jquerydev');
+			modules.push('jquery');
+		}
 	}
 	
 	function updateUrls(){
+		
 		params = '';
-		$('input').each(function(){
-			
-			if ($(this).is(':checked'))
-				params += $(this).val() + '&';
-			
-		});
-	
+		
+		for (var i = 0, curModule; curModule = modules[i++];){
+			params += curModule + '&';
+		}
+		
 		params = params.substring(0, params.length - 1);
+
 		$('#preview-url').val('/builder?print&' + params);
 		$('#download-url').val('/builder?' + params);	
 	}	
+
+	/***********
+	   HELPERS
+	 ***********/
 	
-	updateUrls();
+	if (!Array.indexOf){
+		Array.prototype.indexOf = function(searchedElement){
+			for (var i = 0; i < this.length; i++){
+				if (this[i] === searchedElement)
+					return i;
+			};
+			return -1;
+		};
+	}
+	
+	Array.prototype.remove = function(searchedElement){
+		var i = this.indexOf(searchedElement);
+		if (i != -1)
+			this.splice(i, 1);
+	};
+	
+	/***********
+	    MAIN
+	 ***********/
+	
+	update();
 	
 });
