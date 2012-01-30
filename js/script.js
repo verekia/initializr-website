5259f918036046bb8441924b31894060786420e9
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2012-01-30 17:39:39 +0100

    Added download and preview URLs

--- js/script.js
+++ js/script.js
@@ -1,10 +1,32 @@
-/* Author:
+$(function(){
+	
+	var params;
+	var modules = [];
 
-*/
+	$('input').click(function(){
+		updateUrls();
+		updateModules();
+	});
 
-
-
-
-
-
-
+	
+	function updateModules(){
+		
+	}
+	
+	function updateUrls(){
+		params = '';
+		$('input').each(function(){
+			
+			if ($(this).is(':checked'))
+				params += $(this).val() + '&';
+			
+		});
+	
+		params = params.substring(0, params.length - 1);
+		$('#preview-url').val('/builder?print&' + params);
+		$('#download-url').val('/builder?' + params);	
+	}	
+	
+	updateUrls();
+	
+});
