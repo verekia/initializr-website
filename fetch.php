165c481d272ed5aac9f61afe67513730578e1ad5
Taylor Warden <whotooktwarden@gmail.com>
2015-07-25 15:53:54 -0400

    Create fetch.php
    Fetches the USD price of XRP, fetches the exchange rate to CAD, performs multiplication, and then displays the price of XRP in CAD.  
    
    To apply this for your own legal jurisdiction:
    
    Find your Central Bank's RSS feed which states the daily Close rates relative to the USD.  Get the page's contents and use preg_match to find the string you want to search.  Convert the array into a string using json_encode.  Search the string for the price data using substr an store it in a variable to display it to the user.

---
+++ fetch.php
@@ -0,0 +1,74 @@
+<?php
+
+//$content = file_get_contents("http://www.cba.am/am/SitePages/Default.aspx");
+
+//preg_match('#<b>EUR</b>(.*)<em class="w_50">([0-9\.]*)</em><em class="w_50">([0-9\.]*)</em>#Uis', $content, $EURmatch);
+//preg_match('#<b>GBP</b>(.*)<em class="w_50">([0-9\.]*)</em><em class="w_50">([0-9\.]*)</em>#Uis', $content, $GBPmatch);
+
+//$gbp = $GBPmatch[3];
+//$eur = $EURmatch[3];
+
+//echo "EUR: $eur  GBP: $gbp <br /><br />";
+
+//Above example is from http://stackoverflow.com/questions/7144844/fetching-data-from-another-website-with-php
+
+$content = file_get_contents("http://coinmarketcap.com/");
+$xrppattern = '/\/ripple\/#markets" class="price" data-usd="(\d*\.)?\d+/';
+
+preg_match($xrppattern, $content, $xrpmatch);
+$firstreturn = json_encode($xrpmatch); //converts the array to a string
+//echo "first return is: $firstreturn <br />";  //displays the matched string
+
+$findme = 'd=\"'; //looks for the ending portion of the data-usd string from coinmarketcap.com
+$pos = strpos($firstreturn, $findme);  //Find the position of the above string in the matched case
+//echo "pos: $pos <br />";  //displays the position
+$xrp = substr($firstreturn, ($pos+4), 9); //Gets up to 10 digits of data from the data-usd field
+//echo "str: $xrp <br />"; // displays it to the user
+
+echo "XRP is $xrp USD each.<br />"; //returns XRP with USD price
+
+//Remove comments to debug the array
+//echo "xrp match, regex return: " ;
+//print_r($xrpmatch);
+//echo "<br />";
+//echo "json:", json_encode($xrpmatch), "<br />";
+
+//fetch the USD to CAD exchange rate from http://www.bankofcanada.ca/stats/assets/rates_rss/closing/en_USD_CLOSE.xml
+//multiply the result of $xrp by the result of $exchangeRate to find $cadxrprate
+//CA:\s[0-9.]{2,} first match
+
+$content2 = file_get_contents("http://www.bankofcanada.ca/stats/assets/rates_rss/closing/en_USD_CLOSE.xml");
+$cadpattern = '/CA:\s[0-9.]{2,}/';
+$findcad = '/[0-9.]{2,}/';
+
+preg_match($cadpattern, $content2, $cadmatch);
+$thereturn = json_encode($cadmatch); //converts the array to a string
+//echo "the return is: $thereturn <br />";  //displays the matched string
+//echo "cad match is: $cadmatch <br />";  //displays the matched string 
+$search = json_encode($thereturn);
+//echo "search is: $search <br />";  //displays the matched string
+
+preg_match($findcad, $search, $returnedmatch);
+$cadstr = json_encode($returnedmatch);
+
+$findme = '"'; //looks for the ending portion of the data-usd string from coinmarketcap.com
+$pos = strpos($cadstr, $findme);  //Find the position of the above string in the matched case
+//echo "pos: $pos <br />";  //displays the position
+$cad = substr($cadstr, ($pos+1), 6); //Gets up to 10 digits of data from the data-usd field
+//echo "cadstr: $cadstr <br />"; // displays it to the user
+//echo "cad: $cad <br />"; // displays it to the user
+echo "The CAD to USD exchange rate is currently $cad USD per CAD.<br />"; //returns XRP with USD price
+
+$xrpincad = $xrp * $cad;
+
+echo "Each XRP is $xrpincad in CAD.";
+
+$exchangeRate = $cad;
+$ripplesCAD = $xrpincad;
+$ripplesUSD = $xrp;
+
+return $exchangeRate;
+return $ripplesCAD;
+return $ripplesUSD;
+
+?>
