c24b471578a710338ee8c06921f404d38a30481f
Taylor Warden <whotooktwarden@gmail.com>
2015-08-04 14:06:25 -0400

    Update and rename qgk_sql.sh to qgk_sql.sql
    Re-factoring this sh script into a sql script to be used as input for a shell script which will populate the database with these tables.

---
+++ qgk_sql.sql
@@ -0,0 +1,30 @@
+mysql -D qgk -u qgk_user -pPASSWORD 
+
+use qgk;
+
+CREATE TABLE users (
+	id int NOT NULL AUTO_INCREMENT,
+	email  varchar(60) NOT NULL,
+	ripple varchar(60) NOT NULL,
+	fname varchar(60) NOT NULL,
+	lname varchar(60) NOT NULL,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE public (
+	id int NOT NULL AUTO_INCREMENT,
+	user_id int NOT NULL AUTO_INCREMENT,
+	key varchar(60) NOT NULL,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE private (
+	id int NOT NULL AUTO_INCREMENT,
+	key varchar(60) NOT NULL,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE redeem (
+	id int NOT NULL AUTO_INCREMENT
+	PRIMARY KEY(id)
+);
