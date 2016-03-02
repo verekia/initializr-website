080a0ee7ead8b585578e69beaf5cda709a21de52
Taylor Warden <taylor@xagate.com>
2016-03-02 14:55:58 -0500

    Update pending.php
    Refactor to use blockchain_transactions table and replace boolean false with 0.

--- pending.php
+++ pending.php
@@ -10,21 +10,17 @@ or die ('Error connecting to mysql!  Please check config.php and your database f
 //Use a while loop to print each line by ID then append with the newline character
 //assign SQL to a variable for query execution
 
-$sql = "SELECT id, crypto_symbol, crypto_address, external_address, direction,  timestamp, finished FROM coin_index WHERE finished = false;";
-
+$sql = "SELECT id, crypto_symbol, crypto_address, external_address, is_inbound,  rc_timestamp, finished, bc_blockhash, bc_amount, bc_txid FROM blockchain_transactions WHERE finished = 0 AND bc_txid IS NOT NULL;";
 //connect to the database, execute the $sql, store the returned array within $result
 //script exits gracefully if connection cannot be established
-
 $result = $conn->query($sql);
-
 //if the returned array has data
-
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
         echo "id: " . $row["id"]. " - Crypto Symbol: " . $row["crypto_symbol"]. " - Cryptocurrency Address: " . $row["crypto_address"].
 " - External Address: " . $row["external_address"].
- " - Time-stamp: " . $row["timestamp"]. " - Direction: " . $row["direction"]. " - Finished: " . $row["finished"]. "<br />";
+ " - Time-stamp: " . $row["rc_timestamp"]. " - Is Inbound: " . $row["is_inbound"]. " - Finished: " . $row["finished"]. " - Blockhash: ". $row["bc_blockhash"]. " Amount: " . $row["bc_txid"]. " TxID: " . $row["bc_txid"].  "<br />";
     }
 } else {
           //else there were no results, notify RC operator
